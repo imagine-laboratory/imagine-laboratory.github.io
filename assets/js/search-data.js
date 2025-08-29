8649b4efff8cf40ef62822b9c468905c84a5bc2f
dnnxl <51223285+dnnxl@users.noreply.github.com>
2025-08-29 16:39:30 +0000

    Deploying to gh-pages from @ imagine-laboratory/imagine-laboratory.github.io@aaa107fb8bc9b61cff1a66fb70b4f1ec6d94ebcf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -478,7 +478,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%65%65%70%6C%65%61%72%6E%69%6E%67@%73%61.%75%63%72.%61%63.%63%72", "_blank");
         },
       },{
         id: 'social-inspire',
